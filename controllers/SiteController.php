81114b4111fd8dcf809e880c356d6884a1b2c203
Emerson Pinheiro <emersonline2007@gmail.com>
2019-02-25 23:35:11 -0400

    Yii Framework 2 - Force Download (Forzar descarga de archivos del servidor); Yii Framework 2 - UploadedFile (Subida de archivos)

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -28,6 +28,9 @@ use app\models\FormResetPass;
 
 use app\models\User;
 
+use app\models\FormUpload;
+use yii\web\UploadedFile;
+
 class SiteController extends Controller
 {
     /**
@@ -79,6 +82,90 @@ class SiteController extends Controller
                 ],
             ],
         ];
+    }
+
+    private function downloadFile($dir, $file, $extensions=[])
+    {
+        //Si el directorio existe
+        if (is_dir($dir))
+        {
+            //Ruta absoluta del archivo
+            $path = $dir.$file;
+
+            //Si el archivo existe
+            if (is_file($path))
+            {
+                //Obtener información del archivo
+                $file_info = pathinfo($path);
+                //Obtener la extensión del archivo
+                $extension = $file_info["extension"];
+
+                if (is_array($extensions))
+                {
+                    //Si el argumento $extensions es un array
+                    //Comprobar las extensiones permitidas
+                    foreach($extensions as $e)
+                    {
+                        //Si la extension es correcta
+                        if ($e === $extension)
+                        {
+                            //Procedemos a descargar el archivo
+                            // Definir headers
+                            $size = filesize($path);
+                            header("Content-Type: application/force-download");
+                            header("Content-Disposition: attachment; filename=$file");
+                            header("Content-Transfer-Encoding: binary");
+                            header("Content-Length: " . $size);
+                            // Descargar archivo
+                            readfile($path);
+                            //Correcto
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        //Ha ocurrido un error al descargar el archivo
+        return false;
+    }
+
+    public function actionDownload()
+    {
+        if (Yii::$app->request->get("file"))
+        {
+            //Si el archivo no se ha podido descargar
+            //downloadFile($dir, $file, $extensions=[])
+            if (!$this->downloadFile("archivos/", Html::encode($_GET["file"]), ["pdf", "txt", "doc"]))
+            {
+                //Mensaje flash para mostrar el error
+                Yii::$app->session->setFlash("errordownload");
+            }
+        }
+        return $this->render("download");
+    }
+
+    public function actionUpload()
+    {
+        $model = new FormUpload;
+
+        $msg = null;
+
+        if ($model->load(Yii::$app->request->post()))
+        {
+            $model->file = UploadedFile::getInstances($model, 'file');
+            //Para un input file simple
+            //$model->file = UploadedFile:getInstance($model, 'file');
+            //$file = $model->file;
+            //$file->saveAs(...);
+
+            if ($model->file && $model->validate()) {
+                foreach ($model->file as $file) {
+                    $file->saveAs('archivos/' . $file->baseName . '.' . $file->extension);
+                    $msg = "<p><strong class='label label-info'>Enhorabuena, subida realizada con éxito</strong></p>";
+                }
+            }
+        }
+        return $this->render("upload", ["model" => $model, "msg" => $msg]);
     }
 
     public function actionUser(){
